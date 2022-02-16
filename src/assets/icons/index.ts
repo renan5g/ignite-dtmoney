7a14d3b4a9aa72f550863893e42b37831059cb68
renan5g <renansilva.dev5g@gmail.com>
2022-02-16 13:53:54 -0300

    feat: svgr config and add assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,7 @@
+import CloseIcon from './close.svg?component';
+import IncomeIcon from './income.svg?component';
+import LogoIcon from './logo.svg?component';
+import OutcomeIcon from './outcome.svg?component';
+import TotalIcon from './total.svg?component';
+
+export { CloseIcon, IncomeIcon, LogoIcon, OutcomeIcon, TotalIcon };
